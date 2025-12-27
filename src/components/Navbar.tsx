1bd0d079f1ae0441c3f185e7838da3bfa0fc9da3
iftyy <arifinhudaifty@gmail.com>
2025-12-28 04:19:58 +0600

    unet

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -112,7 +112,7 @@ export default function Navbar() {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <Link href="/" className="text-2xl font-bold text-blue-600 dark:text-blue-400">
-            StudentHub
+            UNET
           </Link>
 
           {/* Desktop Navigation */}
